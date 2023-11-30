7123764c255cd97fa74bc69cc53a4b7006758a98
di3gomil3s <soydiegoff@gmail.com>
2023-11-29 19:24:48 -0600

    agregamos google-site-verification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import NavBarContainer from "@/app/recommendations/components/navBar/NavBarContainer";
 // import Link from "next/link";
 import styles from './page.module.css'
+import Head from 'next/head';
+
+
 
 export default function Home() {
   return (
-    <>    
+    <>
+    <Head>
+          <meta name="google-site-verification" content="WKav3lHrz5lYXdW6XkvigG4CQArrzKtObiBAdGOzNj0" />
+      </Head>    
       <NavBarContainer />
       <main className={styles.home}>
       </main>
