324beb058e7eadecb12dc2b46c95dd39776e80d2
di3gomil3s <soydiegoff@gmail.com>
2023-12-04 13:48:42 -0600

    corrigiendo Scripts Third party

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,8 @@ export const metadata: Metadata = {
   description: 'Compendium of scientific human knowladge',
 }
 
+const GTM_ID = process.env.GTM_ID;
+const GTM_ID_ANA = process.env.GTM_ID_ANA;
 
 export default function RootLayout({
   children,
@@ -43,15 +45,60 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${crimson_text.className} ${montserrat.className}`}>
+      <noscript>
+        <iframe
+          src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        />
+      </noscript>
         {/* <NavBarContainer title="" profileLink='' menuLink=''/> */}
         {children}
         <AnalyticsVercel />
         {/* <ScrollTopButton /> */}
         <Footer />
+
         <Suspense fallback={<div>Loading...</div>}>
           <GoogleAnalytics />
         </Suspense>
-          <GoogleAdsScript/>
+        <GoogleAdsScript/>
+      <Script
+        id="gtm-script"
+        strategy="worker"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `,
+        }}
+      />
+      <Script
+        // src="https://www.googletagmanager.com/gtag/js?id=G-9Z2NG5S0GC"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID_ANA}`}
+        strategy="worker"
+        async
+      />
+      <Script
+        id="google-analytics-script"
+        strategy="worker"
+      >
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GTM_ID_ANA}');
+        `}
+        {/* {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-9Z2NG5S0GC');
+        `} */}
+      </Script>
       </body>
     </html>
   )
