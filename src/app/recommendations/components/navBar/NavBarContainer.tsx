30a02fa8b002911737f989dd9c90a59d6802451f
di3gomil3s <soydiegoff@gmail.com>
2023-12-05 13:17:51 -0600

    componetizando navbar

--- src/app/recommendations/components/navBar/NavBarContainer.tsx
+++ src/app/recommendations/components/navBar/NavBarContainer.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState, useEffect, useCallback, memo, useRef, CSSProperties, FC } from 'react';
+import React, { useState, useEffect, useCallback, memo, useRef, FC, CSSProperties } from 'react';
 import Link from 'next/link';
 import styles from './NavbarContainer.module.css';
 import { Suspense, lazy } from 'react';
@@ -16,22 +16,56 @@ interface NavbarProps {
     active?: boolean;
 }
 
+interface LinkProps {
+    title?: string;
+    path: string;
+    active?: boolean;
+    onClick?: () => void;
+    className?: string;  // Add this line
+}
+const LinkComponent: FC<LinkProps> = ({ title, path, active, onClick }) => (
+    <Link href={path} onClick={onClick} style={{ color: active ? 'var(--color-title1)' : '' }}>
+        {title}
+    </Link>
+);
+
+const NavigationLinks: FC<NavbarProps> = ({ title, title2, title3, domain, active }) => {
+    const generateLink = useCallback((_domain: string | undefined, _title: string | undefined, additionalPath: string = ''): string => {
+        const safeTitle = _title ?? '';
+        return `/recommendations/${toSlug(safeTitle)}${additionalPath}`;
+    }, []);
+
+    return (
+        <div className={styles.contextualLinks}>
+            <LinkComponent title={title} path="/recommendations/" />
+            {title2 && (
+                <>
+                    <span className={styles.padding}>{" > "}</span>
+                    <LinkComponent title={title2} path={generateLink(domain, title2)} active={active} />
+                </>
+            )}
+            {title3 && (
+                <>
+                    <span className={styles.padding}>{">"}</span>
+                    <LinkComponent title={title3} path={generateLink(domain, title2, `/${toSlug(title3)}`)} className={styles.active} />
+                </>
+            )}
+        </div>
+    );
+};
+
+const MenuToggle: FC<{ onClick: () => void; isMenuVisible: boolean }> = ({ onClick, isMenuVisible }) => (
+    <button className={styles.rightIcon} onClick={onClick} aria-label="Open menu">
+        <div tabIndex={0} className={`${styles.iconWrapper} ${styles.crossIcon} ${isMenuVisible ? styles.crossIconOpen : styles.crossIconClosed}`}></div>
+    </button>
+);
+
 const NavBarContainer: FC<NavbarProps> = memo(({ title, title2, title3, domain, active }) => {
     const [isMenuVisible, setIsMenuVisible] = useState<boolean>(false);
     const [lastScrollY, setLastScrollY] = useState<number>(0);
     const [showNavbar, setShowNavbar] = useState<boolean>(true);
     const [scrollPosition, setScrollPosition] = useState<number>(0);
     const wasMenuVisible = useRef<boolean>(isMenuVisible);
-
-    const generateLink = useCallback((_domain: string | undefined, _title: string | undefined, additionalPath: string = ''): string => {
-        const safeTitle = _title ?? '';
-        return `/recommendations/${toSlug(safeTitle)}${additionalPath}`;
-    }, []);
-
-    const closeDropdown = useCallback(() => {
-        const elem = document.activeElement as HTMLElement;
-        elem?.blur();
-    }, []);
 
     const handleScroll = useCallback(() => {
         const currentScrollY = window.scrollY;
@@ -44,16 +78,16 @@ const NavBarContainer: FC<NavbarProps> = memo(({ title, title2, title3, domain, 
         return () => window.removeEventListener('scroll', handleScroll);
     }, [handleScroll]);
 
-    const updateBodyStyle = useCallback((menuVisible: boolean) => {
-        const bodyStyle = document.body.style;
-        bodyStyle.overflow = menuVisible ? 'hidden' : 'auto';
-        bodyStyle.height = menuVisible ? '100vh' : 'auto';
-    }, []);
-
     useEffect(() => {
+        const updateBodyStyle = (menuVisible: boolean) => {
+            const bodyStyle = document.body.style;
+            bodyStyle.overflow = menuVisible ? 'hidden' : 'auto';
+            bodyStyle.height = menuVisible ? '100vh' : 'auto';
+        };
+
         updateBodyStyle(isMenuVisible);
         return () => updateBodyStyle(false);
-    }, [isMenuVisible, updateBodyStyle]);
+    }, [isMenuVisible]);
 
     useEffect(() => {
         if (!isMenuVisible && wasMenuVisible.current) {
@@ -86,28 +120,8 @@ const NavBarContainer: FC<NavbarProps> = memo(({ title, title2, title3, domain, 
     return (
         <div className={styles.container}>
             <div className={`${styles.navbarContainer} ${showNavbar ? '' : styles.hidden}`}>
-                <div className={styles.contextualLinks}>
-                    <Link href="/recommendations/" onClick={closeDropdown}>{title}</Link>
-                    {title2 && (
-                        <>
-                            <span className={styles.padding}>{" > "}</span>
-                            <Link href={generateLink(domain, title2)} style={{ color: active ? 'var(--color-title1)' : '' }} onClick={closeDropdown}>
-                                {title2}
-                            </Link>
-                        </>
-                    )}
-                    {title3 && (
-                        <>
-                            <span className={styles.padding}>{">"}</span>
-                            <Link href={generateLink(domain, title2, `/${toSlug(title3)}`)} className={`${styles.active} 'menu-link'`} onClick={closeDropdown}>
-                                {title3}
-                            </Link>
-                        </>
-                    )}
-                </div>
-                <button className={styles.rightIcon} onClick={toggleMenu}  aria-label="Open menu">
-                    <div tabIndex={0} className={`${styles.iconWrapper} ${styles.crossIcon} ${isMenuVisible ? styles.crossIconOpen : styles.crossIconClosed}`}></div>
-                </button>
+                <NavigationLinks title={title} title2={title2} title3={title3} domain={domain} active={active} />
+                <MenuToggle onClick={toggleMenu} isMenuVisible={isMenuVisible} />
             </div>
             <Suspense fallback={<div>Loading menu...</div>}>
                 <NavigationMenu style={menuStyle} />
