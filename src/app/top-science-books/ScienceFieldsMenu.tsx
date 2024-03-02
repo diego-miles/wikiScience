577e02f45029a15f86b37d6ec51eadfdcfc1a679
di3gomil3s <soydiegoff@gmail.com>
2024-03-02 09:31:59 -0600

    corregido top-science-books

--- src/app/top-science-books/ScienceFieldsMenu.tsx
+++ src/app/top-science-books/ScienceFieldsMenu.tsx
@@ -1,7 +1,5 @@
-"use client"
-import React, { FC, useState, useCallback } from 'react';
+import React, { FC } from 'react';
 import Link from 'next/link';
-import { FaListUl, FaChevronDown, FaChevronRight } from 'react-icons/fa';
 import styles from './ScienceFieldsMenu.module.css';
 
 interface ScienceFieldsMenuProps {
@@ -11,132 +9,35 @@ interface ScienceFieldsMenuProps {
 export const ScienceFieldsMenu: FC<ScienceFieldsMenuProps> = ({ data }) => {
   return (
     <div className={styles.menu}>
-      {data.map((field, index) => (
-        <ExpandableItem key={index} title={field.title} items={field.subFields} />
+      {data.map((field) => (
+        <section key={field.title}>
+          <h2 className={styles.branchTitle}>{field.title.toUpperCase()}</h2>
+          <ul>
+            {field.subFields.map((item) => (
+              <li key={item.title}>
+                <h3 className={styles.subFieldTitle}>
+                  <Link href={`/top-science-books/${toSlug(item.title)}`} className={styles.ScienceFieldsMenu__linkItem}>
+                    {item.title}
+                  </Link>
+                </h3>
+                <ul>
+                  {item.topics.map((topic) => (
+                    <li key={topic} className={styles.ScienceFieldsMenu__linkItem}>
+                      <Link href={`/top-science-books/${toSlug(item.title)}/${toSlug(topic)}`} className={styles.ScienceFieldsMenu__linkItem}>
+                        <h4 className={styles.topicTitle}>{topic}</h4>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ul>
+        </section>
       ))}
     </div>
   );
 };
-
-interface ExpandableItemProps {
-  title: string;
-  items: Wiki.SubTopic[];
-}
-
-const ExpandableItem: FC<ExpandableItemProps> = ({ title, items }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggleOpen = useCallback(() => {
-    setIsOpen(prev => !prev);
-  }, []);
-
-  return (
-    <div className={styles.ScienceFieldsMenu__item}>
-      <button onClick={toggleOpen} className={styles.ScienceFieldsMenu__itemHeader}>
-        <div className={styles.ScienceFieldsMenu__textAndIconWrapper}>
-          <h2 className={styles.branchTitle}>{title.toUpperCase()}</h2>
-          <div className={styles.ScienceFieldsMenu__iconContainer}>
-            {/* <ListIconWithOverlay title={title} /> */}
-          </div>
-        </div>
-        <div className={styles.ScienceFieldsMenu__menuIcon}>
-          {isOpen ? <FaChevronDown /> : <FaChevronRight />}
-        </div>
-      </button>
-      {isOpen && items.map((item, index) => (
-        <ExpandableSubItem key={index} title={item.title} items={item.topics} />
-      ))}
-    </div>
-  );
-};
-
-interface ExpandableSubItemProps {
-  title: string;
-  items: string[];
-}
 
 const toSlug = (title: string) => {
   return title.toLowerCase().replace(/\s+/g, '_');
 };
-
-const ExpandableSubItem: FC<ExpandableSubItemProps> = ({ title, items }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [clickCount, setClickCount] = useState(0); // New state to track click count
-  const toggleOpen = useCallback(() => {
-    setIsOpen(prev => !prev);
-  }, []);
-
-  const handleTitleClick = useCallback(() => {
-    setClickCount(prevCount => prevCount + 1);
-    if (clickCount === 1) {
-      // Navigate to a different URL on the second click
-      window.location.href = `recommendations/${toSlug(title)}`;
-    }
-  }, [clickCount, title]);
-
-  return (
-    <div className={styles.ScienceFieldsMenu__item}>
-      <button onClick={toggleOpen} className={styles.ScienceFieldsMenu__itemHeader}>
-        <div className={styles.ScienceFieldsMenu__textAndIconWrapper}>
-          {/* Modified to handle the new click behavior */}
-          <h3 className={styles.subFieldTitle} onClick={handleTitleClick}>{title}</h3>
-          <div className={styles.ScienceFieldsMenu__iconContainer}>
-            {/* <ListIconWithOverlay title={title} /> */}
-          </div>
-        </div>
-        <div className={styles.ScienceFieldsMenu__menuIcon}>
-          {isOpen ? <FaChevronDown /> : <FaChevronRight />}
-        </div>
-      </button>
-      {isOpen && items.map((item, index) => (
-        // Using Link for navigation
-        <Link key={index} href={`/recommendations/${toSlug(title)}/${toSlug(item)}`} className={styles.ScienceFieldsMenu__linkItem}>
-           {item}
-        </Link>
-      ))}
-    </div>
-  );
-};
-
-
-// interface LinkItemProps {
-//   item: string;
-// }
-
-// const LinkItem: FC<LinkItemProps> = ({ item }) => (
-//   <div className={styles.ScienceFieldsMenu__linkItem}>
-//     <Link href={`/recommendations/branch/${toSlug(item)}`}>
-//       <h4 className={styles.topicTitle}>{item}</h4>
-//     </Link>
-//     <div className={styles.ScienceFieldsMenu__iconContainer}>
-//       {/* <ListIconWithOverlay title={item} /> */}
-//     </div>
-//   </div>
-// );
-
-interface ListIconWithOverlayProps {
-  title: string;
-}
-
-const ListIconWithOverlay: FC<ListIconWithOverlayProps> = ({ title }) => {
-  const [isListOpen, setIsListOpen] = useState(false);
-  const toggleListOpen = useCallback((event: React.MouseEvent) => {
-    event.stopPropagation();
-    setIsListOpen(prev => !prev);
-  }, []);
-
-  return (
-    <>
-      <FaListUl onClick={toggleListOpen} className={styles.ScienceFieldsMenu__icon} />
-      {isListOpen && (
-        <div className={styles.ScienceFieldsMenu__listOverlay}>
-          <p>This is a list content for {title}</p>
-          <button onClick={() => setIsListOpen(false)} className={styles.ScienceFieldsMenu__closeButton}>Cerrar</button>
-        </div>
-      )}
-    </>
-  );
-};
-
-
-
-
