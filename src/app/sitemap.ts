df77d090072e1824deb4353a6da80cd188a26bab
di3gomil3s <soydiegoff@gmail.com>
2024-03-04 14:12:33 -0600

    corregido urls del sitemap5

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,8 @@ import data from '@/components/ScienceFieldsData'; // Asegúrate de que la ruta 
 const baseUrl = 'https://wiki-science.com/top-science-books';
 
 const toSlug = (title: string) => {
-  return title.replace(/[^a-zA-Z0-9 ,'-]/g, "").replace(/ /g, "_").toLowerCase;
+  const slug = title.replace(/[^a-zA-Z0-9 ,'-]/g, "").replace(/ /g, "_");
+  return slug.toLowerCase();
 };
 
 export default function sitemap(): MetadataRoute.Sitemap {
