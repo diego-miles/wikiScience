e61473e8e55eeb75fa5c7b36032b18db2ec9cb72
di3gomil3s <soydiegoff@gmail.com>
2024-03-04 11:48:03 -0600

    corregido url del sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 import data from '@/components/ScienceFieldsData'; // Asegúrate de que la ruta de importación sea correcta
 
-const baseUrl = 'https://wiki-science.com/recommendations';
+const baseUrl = 'https://wiki-science.com/top-science-books';
 
 const toSlug = (title: string) => {
   return title.replace(/[^a-zA-Z0-9 ,'-]/g, "").replace(/ /g, "%20");
