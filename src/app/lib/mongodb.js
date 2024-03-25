7615b5cca0d7e94aa97c1de33b44c5eab5e11224
di3gomil3s <soydiegoff@gmail.com>
2024-03-25 00:16:26 -0600

    falta corregir syllabus

--- src/app/lib/mongodb.js
+++ src/app/lib/mongodb.js
@@ -1,32 +1,37 @@
-// lib/mongodb.js
-import mongoose from "mongoose"
+// // lib/mongodb.js
+// import mongoose from "mongoose"
 
-const MONGODB_URI = process.env.DATABASE_URI;
+// const MONGODB_URI = process.env.DATABASE_URI;
 
-const connect = async () => {
-    const connectionState = mongoose.connection.readyState;
-    if(connectionState === 1) {
-        console.log("already connected")
-        return;
-    }
+// const connect = async () => {
+//     const connectionState = mongoose.connection.readyState;
+//     if(connectionState === 1) {
+//         console.log("already connected")
+//         return;
+//     }
 
-    if(connectionState === 2) {
-        console.log("Connecting...")
-        return;
-    }
-    try {
-        mongoose.connect(MONGODB_URI, {
-            dbName: "wikiScience",
-            bufferCommands: false
-        })
-        console.log("connected")
-    } catch (error) {
-        console.log("Error connecting to databse", error);
-        throw new Error("Error in connecting Database")
-    }
-};
+//     if(connectionState === 2) {
+//         console.log("Connecting...")
+//         return;
+//     }
+//     try {
+//         mongoose.connect(MONGODB_URI, {
+//             dbName: "wikiScience",
+//             bufferCommands: false
+//         })
+//         console.log("connected")
+//     } catch (error) {
+//         console.log("Error connecting to databse", error);
+//         throw new Error("Error in connecting Database")
+//     }
+// };
 
-export default connect;
+// export default connect;
+
+
+
+
+
 
 // const MONGODB_DB = process.env.MONGODB_DB;
 
