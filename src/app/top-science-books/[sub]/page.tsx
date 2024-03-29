2062ee95ccd1506ec0d4f0c64e24179e947bb4de
di3gomil3s <soydiegoff@gmail.com>
2024-03-29 15:30:34 -0600

    probando bug

--- src/app/top-science-books/[sub]/page.tsx
+++ src/app/top-science-books/[sub]/page.tsx
@@ -4,9 +4,6 @@ import ContextSpace from './components/ContextSpace';
 import ArticleTitle from './components/ArticleTitle';
 import LocalContextLinks from './components/LocalContextLinks';
 import BookRecommendation from './components/BookRecommendation';
-import { cache } from "react";
-import { Metadata } from 'next';
-import { notFound } from 'next/navigation';
 import styles from './page.module.css'
 import ScrollTopButton from '@/components/ScrollTopButton'
 
@@ -18,7 +15,6 @@ interface SubFieldPageProps {
 
 const prisma = new PrismaClient();
 
-
 const formatTitleForURL = (title: string) => {
     return title
         .replace(/&/g, "%26") // Replace '&' with '%26'
@@ -26,7 +22,7 @@ const formatTitleForURL = (title: string) => {
         .replace(/ /g, "+"); // Replace spaces with '%20'
 };
 
-const getSubFieldRecommendation = cache(async (sub: string) => {
+const getSubFieldRecommendation = async (sub: string) => {
     const subFieldData = await prisma.subFieldRecommendation.findUnique({
         where: { slug: sub },
         include: {
@@ -36,51 +32,36 @@ const getSubFieldRecommendation = cache(async (sub: string) => {
         }
     });
 
-    if (!subFieldData) notFound();
+    if (!subFieldData) throw new Error('SubField data not found');
     return subFieldData;
-});
-
-// export async function generateMetadata(
-//     { params: { subfield } }: SubFieldPageProps): Promise<Metadata> {
-//     const subFieldData = await getSubFieldRecommendation(subfield);
-
-//     const images = subFieldData.books.map(book => ({
-//         url: `${formatTitleForURL(book.englishTitle)}.png`
-//     }));
-
-//     const keywords = subFieldData.books.flatMap(book => book.keywords || []);
-//     const uniqueKeywords = Array.from(new Set(keywords));
-//     const description = `Dive into the internet curated, often updated, list of the top science books on ${subFieldData.subField}. From groundbreaking discoveries to the fundamentals of the universe, explore books that have shaped our understanding of science`;
-
-//     return {
-//         title: subFieldData?.subField,
-//         description: description,
-//         openGraph: {
-//             images: images
-//         },
-//         keywords: uniqueKeywords.join(', ')
-//     };
-// }
+};
 
 async function SubFieldRecommendationPage({ params: { sub } }: SubFieldPageProps) {
-    const subFieldData = await getSubFieldRecommendation(sub);
+    let subFieldData;
+    try {
+        subFieldData = await getSubFieldRecommendation(sub);
+    } catch (error) {
+        // Handle the error (e.g., display a message or redirect)
+        console.error(error);
+        return <div>Error loading subfield data</div>;
+    }
 
-    const bookLinks = subFieldData?.books?.map(book => ({
+    const bookLinks = subFieldData.books.map(book => ({
         text: book.englishTitle,
         id: book.englishTitle.replace(/\s+/g, '-').toLowerCase()
     }));
 
-    const bookRecommendations = subFieldData?.books?.map((book, index) =>
+    const bookRecommendations = subFieldData.books.map((book, index) =>
         <BookRecommendation key={book.englishTitle} book={book} priority={index === 0} syllabus={book.syllabus} />
     );
 
     return (
         <div className={styles.wrapper}>
-            <NavBar title={subFieldData?.field} title2={subFieldData?.subField} domain="www.wiki-science.com/" active={true} menuPath='./NavigationMenu'/>
+            <NavBar title={subFieldData.field} title2={subFieldData.subField} domain="www.wiki-science.com/" active={true} menuPath='./NavigationMenu'/>
             <main>
                 <ContextSpace />
-                <ArticleTitle topic={subFieldData?.subField} />
-                <LocalContextLinks links={bookLinks || []} />
+                <ArticleTitle topic={subFieldData.subField} />
+                <LocalContextLinks links={bookLinks} />
                 {bookRecommendations}
                 <div className='globalSpace'></div>
                 <div className='globalSpace'></div>
