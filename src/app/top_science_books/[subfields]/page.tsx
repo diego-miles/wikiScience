2c0962eec03e2a0cf983bf492adf67793a94e404
di3gomil3s <soydiegoff@gmail.com>
2024-03-30 23:32:31 -0600

    buscando el bug

--- src/app/top_science_books/[subfields]/page.tsx
+++ src/app/top_science_books/[subfields]/page.tsx
@@ -1,11 +1,11 @@
 import { PrismaClient } from '@prisma/client';
-// import NavBar from '@/components/NavbarContainer';
+import NavBar from '@/components/NavbarContainer';
 // import ContextSpace from '@/components/books-components/ContextSpace';
 import ArticleTitle from '@/components/books-components/ArticleTitle';
-// import LocalContextLinks from '@/components/books-components/LocalContextLinks';
-// import BookRecommendation from '@/components/books-components/BookRecommendation';
+import LocalContextLinks from '@/components/books-components/LocalContextLinks';
+import BookRecommendation from '@/components/books-components/BookRecommendation';
 import { cache } from "react";
-// import { Metadata } from 'next';
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 // import ScrollTopButton from '@/components/ScrollTopButton';
 // import dynamic from 'next/dynamic';
@@ -42,18 +42,18 @@ async function Page({ params: { subfields } }: SubFieldPageProps) {
         id: book.englishTitle.replace(/\s+/g, '-').toLowerCase(),
     })) || [];
 
-    // const bookRecommendations = subFieldData?.books?.map((book, index) => (
-    //     <BookRecommendation key={book.englishTitle} book={book} syllabus={book.syllabus || {}} priority={index === 0} />
-    // )) || [];
+    const bookRecommendations = subFieldData?.books?.map((book, index) => (
+        <BookRecommendation key={book.englishTitle} book={book} syllabus={book.syllabus || {}} priority={index === 0} />
+    )) || [];
 
     return (
         <div>
-            {/* <NavBar title={subFieldData?.field} title2={subFieldData?.subField} domain="www.wiki-science.com/" active={true} menuPath='./NavigationMenu'/> */}
+            <NavBar title={subFieldData?.field} title2={subFieldData?.subField} domain="www.wiki-science.com/" active={true} menuPath='./NavigationMenu'/>
             <main>
                 {/* <ContextSpace /> */}
                 <ArticleTitle topic={subFieldData?.subField} />
-                {/* <LocalContextLinks links={bookLinks || []} /> */}
-                {/* {bookRecommendations} */}
+                <LocalContextLinks links={bookLinks || []} />
+                {bookRecommendations}
                 {/* <div className='globalSpace'></div> */}
                 {/* <div className='globalSpace'></div> */}
                 {/* <ScrollTopButton /> */}
