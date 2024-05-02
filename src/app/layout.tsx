739642ad951c865e6b37dff131f5974b2ff6d5f8
di3gomil3s <soydiegoff@gmail.com>
2024-05-02 03:23:49 -0600

    turning to light mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -69,7 +69,7 @@ export default function RootLayout({
         </noscript>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
