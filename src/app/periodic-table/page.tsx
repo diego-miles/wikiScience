18fc8e13b4dc183465bd18b0ed8aaaa8e49715ac
di3gomil3s <soydiegoff@gmail.com>
2024-05-20 11:13:58 -0600

    context hover ready, periodic table styles corrections

--- src/app/periodic-table/page.tsx
+++ src/app/periodic-table/page.tsx
@@ -15,7 +15,7 @@ const Home: React.FC = () => {
       <h1 className=" ">Periodic Table of the Chemical Elements</h1>
 
       <div className='absolute left-0 right-0 overflow-auto md:overflow-hidden '>
-        <div className='mx-auto   w-full max-w-[115rem] max-h-40  text-center'>
+        <div className='mx-auto   w-fit  max-h-40  text-center'>
             <ChemicalFamilies></ChemicalFamilies>
           <HydrogenElement></HydrogenElement>
             <States></States>
