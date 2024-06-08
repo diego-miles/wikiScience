12daec030bf0f952006aab971379c8b106d2e922
di3gomil3s <soydiegoff@gmail.com>
2024-06-08 07:14:56 -0600

    deliting prisma and commenting

--- src/app/top-science-books/[slugsub]/[slug]/page.tsx
+++ src/app/top-science-books/[slugsub]/[slug]/page.tsx
@@ -1,108 +1,108 @@
-import { PrismaClient } from '@prisma/client';
-import NavBar from '@/components/navigation/NavbarContainer';
-import ContextSpace from '@/components/books-components/ContextSpace';
-import ArticleTitle from '@/components/books-components/ArticleTitle';
-import LocalContextLinks from '@/components/books-components/LocalContextLinks';
-import BookRecommendation from '@/components/books-components/BookRecommendation';
-// import { cache } from 'react';
-import { Metadata } from 'next';
-import { notFound } from 'next/navigation';
-import ScrollTopButton from '@/components/ScrollTopButton';
-import { unstable_cache } from 'next/cache';
+// import { PrismaClient } from '@prisma/client';
+// import NavBar from '@/components/navigation/NavbarContainer';
+// import ContextSpace from '@/components/books-components/ContextSpace';
+// import ArticleTitle from '@/components/books-components/ArticleTitle';
+// import LocalContextLinks from '@/components/books-components/LocalContextLinks';
+// import BookRecommendation from '@/components/books-components/BookRecommendation';
+// // import { cache } from 'react';
+// import { Metadata } from 'next';
+// import { notFound } from 'next/navigation';
+// import ScrollTopButton from '@/components/ScrollTopButton';
+// import { unstable_cache } from 'next/cache';
 
 
 
 
-type OGImage = {
-    url: string;
-};
+// type OGImage = {
+//     url: string;
+// };
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-interface ProductPageProps {
-    params: {
-        slug: string;
-    };
-}
+// interface ProductPageProps {
+//     params: {
+//         slug: string;
+//     };
+// }
 
-const formatTitleForAmazonImageURL = (title: string) => {
-    return title
-        .replace(/[^a-zA-Z0-9 ,'&-]/g, "")
-        .replace(/&/g, "%26")
-        .replace(/ /g, "+");
-};
-
-
-const getRecommendation = unstable_cache(async (slug: string) => {
-    const recommendations = await prisma.topicRecommendation.findUnique({
-        where: { slug: slug },
-        include: {
-            books: true
-        },
-    });
-
-    if (!recommendations) notFound();
-
-    return recommendations;
-});
+// const formatTitleForAmazonImageURL = (title: string) => {
+//     return title
+//         .replace(/[^a-zA-Z0-9 ,'&-]/g, "")
+//         .replace(/&/g, "%26")
+//         .replace(/ /g, "+");
+// };
 
 
-export async function generateMetadata({
-    params: { slug },
-}: ProductPageProps): Promise<Metadata> {
-    const recommendations = await getRecommendation(slug);
+// const getRecommendation = unstable_cache(async (slug: string) => {
+//     const recommendations = await prisma.topicRecommendation.findUnique({
+//         where: { slug: slug },
+//         include: {
+//             books: true
+//         },
+//     });
+
+//     if (!recommendations) notFound();
+
+//     return recommendations;
+// });
+
+
+// export async function generateMetadata({
+//     params: { slug },
+// }: ProductPageProps): Promise<Metadata> {
+//     const recommendations = await getRecommendation(slug);
 
 
 
 
-    const images = (recommendations?.books?.map((book) => ({
-        url: `${formatTitleForAmazonImageURL(book.englishTitle)}.png` || ''
-    })) || []).filter(image => image.url !== null) as OGImage[];
+//     const images = (recommendations?.books?.map((book) => ({
+//         url: `${formatTitleForAmazonImageURL(book.englishTitle)}.png` || ''
+//     })) || []).filter(image => image.url !== null) as OGImage[];
 
-    const keywords = recommendations?.books?.flatMap((book) => book.keywords || []) || [];
-    const uniqueKeywords = Array.from(new Set(keywords));
-    const description = `Explore our expertly curated and constantly updated list of the top science books on ${recommendations?.topic} for 2024. Discover groundbreaking works that delve into the universe's mysteries and the most significant scientific discoveries. These essential reads offer in-depth knowledge, insightful analysis, and innovative perspectives for both enthusiasts and scholars alike.`;
+//     const keywords = recommendations?.books?.flatMap((book) => book.keywords || []) || [];
+//     const uniqueKeywords = Array.from(new Set(keywords));
+//     const description = `Explore our expertly curated and constantly updated list of the top science books on ${recommendations?.topic} for 2024. Discover groundbreaking works that delve into the universe's mysteries and the most significant scientific discoveries. These essential reads offer in-depth knowledge, insightful analysis, and innovative perspectives for both enthusiasts and scholars alike.`;
 
-    return {
-        title: `Best ${recommendations?.topic} books of all time (2024)`,
-        description: description,
-        openGraph: {
-            images: images,
-        },
-        keywords: uniqueKeywords.join(', '),
-    };
-}
+//     return {
+//         title: `Best ${recommendations?.topic} books of all time (2024)`,
+//         description: description,
+//         openGraph: {
+//             images: images,
+//         },
+//         keywords: uniqueKeywords.join(', '),
+//     };
+// }
 
 
-async function RecommendationPage({
-    params: { slug },
-}: ProductPageProps) {
-    const recommendations = await getRecommendation(slug);
+// async function RecommendationPage({
+//     params: { slug },
+// }: ProductPageProps) {
+//     const recommendations = await getRecommendation(slug);
 
-    const bookLinks = recommendations?.books?.map((book) => ({
-        text: book.englishTitle,
-        id: book.englishTitle.replace(/\s+/g, '-').toLowerCase(),
-    })) || [];
+//     const bookLinks = recommendations?.books?.map((book) => ({
+//         text: book.englishTitle,
+//         id: book.englishTitle.replace(/\s+/g, '-').toLowerCase(),
+//     })) || [];
 
-    const bookRecommendations = recommendations.books.map((book, index) => (
-        <BookRecommendation key={book.englishTitle} book={book} priority={index === 0} />
-    )) || [];
+//     const bookRecommendations = recommendations.books.map((book, index) => (
+//         <BookRecommendation key={book.englishTitle} book={book} priority={index === 0} />
+//     )) || [];
 
-    return (
-        <div>
-            {/* <NavBar title={recommendations?.field} title2={recommendations?.subField} title3={recommendations?.topic} domain="www.wiki-science.com/" menuPath='./NavigationMenu' /> */}
-            <main>
-                <ContextSpace />
-                <ArticleTitle topic={recommendations?.topic} />
-                <LocalContextLinks links={bookLinks || []} />
-                    <div className=' py-20 max-w-[75rem] mx-auto    w-full  rounded-2xl pb-9'>
-                        {bookRecommendations}
-                    </div>
+//     return (
+//         <div>
+//             {/* <NavBar title={recommendations?.field} title2={recommendations?.subField} title3={recommendations?.topic} domain="www.wiki-science.com/" menuPath='./NavigationMenu' /> */}
+//             <main>
+//                 <ContextSpace />
+//                 <ArticleTitle topic={recommendations?.topic} />
+//                 <LocalContextLinks links={bookLinks || []} />
+//                     <div className=' py-20 max-w-[75rem] mx-auto    w-full  rounded-2xl pb-9'>
+//                         {bookRecommendations}
+//                     </div>
 
-                <ScrollTopButton />
-            </main>
-        </div>
-    );
-}
+//                 <ScrollTopButton />
+//             </main>
+//         </div>
+//     );
+// }
 
-export default RecommendationPage;
+// export default RecommendationPage;
--- src/app/top-science-books/[slugsub]/page.tsx
+++ src/app/top-science-books/[slugsub]/page.tsx
@@ -1,123 +1,123 @@
-import { PrismaClient } from '@prisma/client';
-import NavBar from '@/components/navigation/NavbarContainer';
-import ContextSpace from '@/components/books-components/ContextSpace';
-import ArticleTitle from '@/components/books-components/ArticleTitle';
-import LocalContextLinks from '@/components/books-components/LocalContextLinks';
-import BookRecommendation from '@/components/books-components/BookRecommendation';
+// import { PrismaClient } from '@prisma/client';
+// import NavBar from '@/components/navigation/NavbarContainer';
+// import ContextSpace from '@/components/books-components/ContextSpace';
+// import ArticleTitle from '@/components/books-components/ArticleTitle';
+// import LocalContextLinks from '@/components/books-components/LocalContextLinks';
+// import BookRecommendation from '@/components/books-components/BookRecommendation';
+// // import { unstable_cache } from 'next/cache';
+// import { Metadata } from 'next';
+// import { notFound } from 'next/navigation';
+// import ScrollTopButton from '@/components/ScrollTopButton';
+// import { Book } from '@prisma/client'; // Import Ratings from Prisma schema
 // import { unstable_cache } from 'next/cache';
-import { Metadata } from 'next';
-import { notFound } from 'next/navigation';
-import ScrollTopButton from '@/components/ScrollTopButton';
-import { Book } from '@prisma/client'; // Import Ratings from Prisma schema
-import { unstable_cache } from 'next/cache';
 
-interface SubFieldPageProps {
-    params: {
-        slugsub: string;
-    }
-}
+// interface SubFieldPageProps {
+//     params: {
+//         slugsub: string;
+//     }
+// }
 
 
-interface ProductPageProps {
-    params: {
-        slugsub: string;
-    }
-}
+// interface ProductPageProps {
+//     params: {
+//         slugsub: string;
+//     }
+// }
 
-type OGImage = {
-    url: string;
-};
+// type OGImage = {
+//     url: string;
+// };
 
 
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
 
 
 
 
-const getSubFieldRecommendation = unstable_cache(async (slugsub: string) => {
-    const subFieldData = await prisma.subFieldRecommendation.findUnique({
-        where: { slug : slugsub },
-        include: {
-            books: true
-        }
-    });
+// const getSubFieldRecommendation = unstable_cache(async (slugsub: string) => {
+//     const subFieldData = await prisma.subFieldRecommendation.findUnique({
+//         where: { slug : slugsub },
+//         include: {
+//             books: true
+//         }
+//     });
 
-    if (!subFieldData) notFound();
-    return subFieldData;
-});
-
-
-
-const formatTitleForAmazonImageURL = (title: string) => {
-  return title
-    .replace(/[^a-zA-Z0-9 ,'&-]/g, "")
-    .replace(/&/g, "%26")
-    .replace(/ /g, "+");
-};
+//     if (!subFieldData) notFound();
+//     return subFieldData;
+// });
 
 
 
-export async function generateMetadata({
-    params: { slugsub },
-}: ProductPageProps): Promise<Metadata> {
-    const recommendations = await getSubFieldRecommendation(slugsub);
+// const formatTitleForAmazonImageURL = (title: string) => {
+//   return title
+//     .replace(/[^a-zA-Z0-9 ,'&-]/g, "")
+//     .replace(/&/g, "%26")
+//     .replace(/ /g, "+");
+// };
 
-    const images = (recommendations?.books?.map((book) => ({
-        url: `${formatTitleForAmazonImageURL(book.englishTitle)}.png` || '' 
-    })) || []).filter(image => image.url !== null) as OGImage[];
 
-    const keywords = recommendations?.books?.flatMap((book) => book.keywords || []) || [];
-    const uniqueKeywords = Array.from(new Set(keywords));
-    const description = `Explore our expertly curated and constantly updated list of the top science books on ${recommendations?.subField} for 2024. Discover groundbreaking works that delve into the universe's mysteries and the most significant scientific discoveries. These essential reads offer in-depth knowledge, insightful analysis, and innovative perspectives for both enthusiasts and scholars alike.`;
 
-    return {
-        title: `Best ${recommendations?.subField} books of all time (2024)` ,
-        description: description,
-        openGraph: {
-            images: images,
-        },
-        keywords: uniqueKeywords.join(', '),
-    };
-}
+// export async function generateMetadata({
+//     params: { slugsub },
+// }: ProductPageProps): Promise<Metadata> {
+//     const recommendations = await getSubFieldRecommendation(slugsub);
+
+//     const images = (recommendations?.books?.map((book) => ({
+//         url: `${formatTitleForAmazonImageURL(book.englishTitle)}.png` || '' 
+//     })) || []).filter(image => image.url !== null) as OGImage[];
+
+//     const keywords = recommendations?.books?.flatMap((book) => book.keywords || []) || [];
+//     const uniqueKeywords = Array.from(new Set(keywords));
+//     const description = `Explore our expertly curated and constantly updated list of the top science books on ${recommendations?.subField} for 2024. Discover groundbreaking works that delve into the universe's mysteries and the most significant scientific discoveries. These essential reads offer in-depth knowledge, insightful analysis, and innovative perspectives for both enthusiasts and scholars alike.`;
+
+//     return {
+//         title: `Best ${recommendations?.subField} books of all time (2024)` ,
+//         description: description,
+//         openGraph: {
+//             images: images,
+//         },
+//         keywords: uniqueKeywords.join(', '),
+//     };
+// }
 
 
 
 
 
 
-async function Page({ params: { slugsub } }: SubFieldPageProps) {
-    const subFieldData = await getSubFieldRecommendation(slugsub);
+// async function Page({ params: { slugsub } }: SubFieldPageProps) {
+//     const subFieldData = await getSubFieldRecommendation(slugsub);
 
-    const bookLinks = subFieldData.books.map((book: { englishTitle: string; }) => ({
-        text: book.englishTitle,
-        id: book.englishTitle.replace(/\s+/g, '-').toLowerCase(),
-    })) || [];
+//     const bookLinks = subFieldData.books.map((book: { englishTitle: string; }) => ({
+//         text: book.englishTitle,
+//         id: book.englishTitle.replace(/\s+/g, '-').toLowerCase(),
+//     })) || [];
 
-    const bookRecommendations = subFieldData.books.map((book, index) => (
-        <BookRecommendation key={book.englishTitle} book={book}  priority={index === 0} />
+//     const bookRecommendations = subFieldData.books.map((book, index) => (
+//         <BookRecommendation key={book.englishTitle} book={book}  priority={index === 0} />
 
-    )) || [];
+//     )) || [];
 
-    return (
-        <div>
-            {/* <NavBar title={subFieldData.field} title2={subFieldData.subField} domain="www.wiki-science.com/" menuPath='./NavigationMenu'/> */}
-            <main>
-                <ContextSpace />
-                <ArticleTitle topic={subFieldData.subField || ''} />
-                <LocalContextLinks links={bookLinks || []} />
-                <div className=''>
-                    <div className=' mt-20 max-w-[75rem] mx-auto    w-full   rounded-2xl pt-4  '>
-                        {bookRecommendations}
-                    </div>
-                </div>
-                <div className='globalSpace'></div>
-                <div className='globalSpace'></div>
-                <ScrollTopButton />
-            </main>
-        </div>
-    );
-}
+//     return (
+//         <div>
+//             {/* <NavBar title={subFieldData.field} title2={subFieldData.subField} domain="www.wiki-science.com/" menuPath='./NavigationMenu'/> */}
+//             <main>
+//                 <ContextSpace />
+//                 <ArticleTitle topic={subFieldData.subField || ''} />
+//                 <LocalContextLinks links={bookLinks || []} />
+//                 <div className=''>
+//                     <div className=' mt-20 max-w-[75rem] mx-auto    w-full   rounded-2xl pt-4  '>
+//                         {bookRecommendations}
+//                     </div>
+//                 </div>
+//                 <div className='globalSpace'></div>
+//                 <div className='globalSpace'></div>
+//                 <ScrollTopButton />
+//             </main>
+//         </div>
+//     );
+// }
 
-export default Page;
+// export default Page;
