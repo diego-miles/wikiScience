a88f3b58d9b7b378c7e7826806a207c0a598ff67
di3gomil3s <soydiegoff@gmail.com>
2024-06-16 03:55:24 -0600

    changed environment names

--- src/app/api/audio/[slug]/route.ts
+++ src/app/api/audio/[slug]/route.ts
@@ -3,17 +3,17 @@ import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 const s3Client = new S3Client({
-  region: process.env.NEXT_PUBLIC_AWS_REGION,
+  region: process.env.AWS_REGION,
   credentials: {
-    accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
 });
 
 export async function GET(req: NextRequest, { params: { slug } }: { params: { slug: string } }) {
   try {
     const command = new GetObjectCommand({
-      Bucket: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME!,
+      Bucket: process.env.AWS_S3_BUCKET_NAME!,
       Key: `${slug}.mp3`,
     });
 
